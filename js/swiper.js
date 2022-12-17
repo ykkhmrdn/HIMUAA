d49f4b50bafd67377d3c922f5029b82dd74fced9
zyxkoo15 <ykkhmrdn@gmail.com>
2022-12-17 20:10:25 +0700

    swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,15 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
+  effect: "coverflow",
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -36,3 +42,5 @@ var swiper = new Swiper(".mySwiper", {
 
 }
   });
+
+  
